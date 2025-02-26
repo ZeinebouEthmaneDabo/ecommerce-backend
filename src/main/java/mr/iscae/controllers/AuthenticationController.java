cae1c843e50e5501e3985135f358d53ab95ce07e
Dabo <zeineboudabo2003@gmail.com>
2025-02-26 12:35:46 +0100

    add CrossOrigin

--- src/main/java/mr/iscae/controllers/AuthenticationController.java
+++ src/main/java/mr/iscae/controllers/AuthenticationController.java
@@ -8,15 +8,13 @@ import mr.iscae.dtos.requests.RegisterRequest;
 import mr.iscae.dtos.responses.AuthenticationResponse;
 import mr.iscae.services.AuthenticationService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class AuthenticationController {
 
     private final AuthenticationService service;
--- src/main/java/mr/iscae/controllers/OrderController.java
+++ src/main/java/mr/iscae/controllers/OrderController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/orders")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class OrderController {
     private final OrderService orderService;
 
